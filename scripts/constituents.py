86791ffb694324dc1776153c103399d9a7c3aa33
abhandary <akshay.bhandary@gmail.com>
2018-06-08 01:33:19 -0700

    modified constituents.py to produce a form with useful links to morningstar

--- scripts/constituents.py
+++ scripts/constituents.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import csv
 from os import mkdir
+from yattag import Doc
 from os.path import exists, join
-import urllib.request as request
+import urllib
 
 
 datadir = join('..', 'data')
@@ -16,7 +17,7 @@ source = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 cache = join('tmp', 'List_of_S%26P_500_companies.html')
 
 def retrieve():
-    request.urlretrieve(source, cache)
+    urllib.urlretrieve(source, cache)
 
 def extract():
     source_page = open(cache).read()
@@ -35,17 +36,56 @@ def extract():
         fields = row.findAll('td')
         if fields:
             symbol = fields[0].string
+            exchange = 'XNAS' if fields[0].a.get('href').find('nasdaq') != -1 else 'XNYS'
+            main_page = 'http://www.morningstar.com/stocks/' + exchange + '/' + symbol + '/quote.html'
+            profile_page = main_page + '#sal-components-company-profile'
+            price_vs_fair_value = main_page + '#sal-components-price-fairvalue'
+            financials = main_page + '#sal-components-financials'
+            valuation = main_page + '#sal-components-valuation'
+            performance = main_page + '#sal-components-oper-perf'
+
             # fix as now they have links to the companies on WP
             name = ' '.join(fields[1].stripped_strings)
             sector = fields[3].string
-            records.append([symbol, name, sector])
+            records.append([symbol, name, exchange, sector, profile_page, price_vs_fair_value, financials, valuation, performance])
 
     header = ['Symbol', 'Name', 'Sector']
     writer = csv.writer(open('../data/constituents.csv', 'w'), lineterminator='\n')
     writer.writerow(header)
     # Sorting ensure easy tracking of modifications
     records.sort(key=lambda s: s[1].lower())
-    writer.writerows(records)    
+    writer.writerows(records)
+
+    with open("template.html") as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+
+    doc, tag, text, line = Doc().ttl()
+    with tag('tbody'):
+        for record in records:
+            with tag('tr'):
+                with tag('td'):
+                    text(record[0])
+                with tag('td'):
+                    text(record[1])
+                with tag('td'):
+                    text(record[2])
+                with tag('td'):
+                    text(record[3])
+                with tag('td'):
+                    line('a', 'link', href=record[4])
+                with tag('td'):
+                    line('a', 'link', href=record[5])
+                with tag('td'):
+                    line('a', 'link', href=record[6])
+                with tag('td'):
+                    line('a', 'link', href=record[7])
+                with tag('td'):
+                    line('a', 'link', href=record[8])
+
+    final = soup.prettify().replace("##placeholder##", doc.getvalue())
+
+    with open('../data/index.html', 'w') as file_:
+        file_.write(final)
 
 def process():
     retrieve()
@@ -53,4 +93,3 @@ def process():
 
 if __name__ == '__main__':
     process()
-
